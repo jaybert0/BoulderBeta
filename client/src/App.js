5858d2758e163ebeccae65dbe332fb2cee7a17a9
J Lee <jay.j.lee.1@gmail.com>
2022-03-16 14:11:11 -0400

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,50 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import { Alert } from '@mui/material';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Container from '@mui/material/Container';
+
+
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  function handleLogOutClick(){
+    fetch("/logout",{
+        method: "DELETE"
+    }).then((r) => {
+        if(r.ok){
+            setUser(null);
+        }
+    });
+    // Navigate to home page after logout and clear history
+    navigate("/");
+}
+
+if (!user) return (
+  <>
+  <Container>
+    <Alert className="mt-3" variant="primary" >Please Login OR Signup To Create A New Account</Alert>
+  </Container>
+  <Login onLogin={setUser}/>
+  </>
+)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
