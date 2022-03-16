e91b0525e99fd9d7908e313034a284d25ce9d8be
J Lee <jay.j.lee.1@gmail.com>
2022-03-16 14:00:18 -0400

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+    has_secure_password
+    
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    
+    def reset_password!(password)
+        self.password = password
+        save!
+    end
+    
 end
