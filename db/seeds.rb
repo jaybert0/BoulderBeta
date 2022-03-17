cdd29f0dd64a69fb6e32937fcdc07940c2637b36
J Lee <jay.j.lee.1@gmail.com>
2022-03-17 14:15:40 -0400

    associations good!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'date'
+puts "🌱 Seeding spices..."
 
-User.create(username: 'jay',
+User.create(
+    username: 'jay',
     password: '123',
-    email: 'jay'
+    email: 'jay',
     admin: true,
 )
-Tech.create(handholds: 'jug',
-hold_description: 'grab it around')
-Problem.create(tech_id: 1,
+Tech.create(
+    handholds: 'jug',
+    hold_description: 'grab it around')
+Problem.create(
+    tech_id: 1,
 location_id: 1,
 grip_color: "pink",
-problem_description: 'great'
-difficulty: 1)
+problem_description: 'great',
+difficulty: 1,
+rating: 4
+)
+Location.create(
+    location: 1,
+    loc_description: "End of the cave"
+)
+Climbproblem.create(
+    user_id: 1,
+    problem_id: 1,
+    favorite: true, 
+    in_progress: false,
+    feedback: "Great",
+    rating: 4
+)
+puts "🌱 Seeded spices..."
