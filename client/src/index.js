1997c35aa82d21a7ee0e2bb91e57403027ff0c80
J Lee <jay.j.lee.1@gmail.com>
2022-03-18 18:43:59 -0400

    setup redux store. need to build counter

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
+const store = createStore(counter,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <BrowserRouter>
+  <Provider store={store}></Provider>
     <App />
+  <Provider />
   </BrowserRouter>,
   document.getElementById('root')
 );
