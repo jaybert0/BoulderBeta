fb3e07a3e9ac0d2807257d0b0ff17852a5676f48
J Lee <jay.j.lee.1@gmail.com>
2022-03-18 10:53:58 -0400

    location controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,33 @@
 class LocationsController < ApplicationController
+    def index
+        render json: Location.all,  status: :ok
+      end
+      
+      def show
+        location = Location.find(params[:id])
+        render json: location, status: :ok
+      end
+    
+      def update
+        location = Location.find(params[:id])
+        location.update!(location_params)
+        render json: location, status: :ok
+      end
+    
+      def create
+        location = Location.create!(location_params)
+        render json: location, status: :created
+      end
+    
+      def destroy
+        location = Location.find(params[:id])
+        location.destroy
+        head :no_content
+      end
+    
+      private
+        def location_params
+            params.permit(:location, :loc_description)
+        end
+
 end
