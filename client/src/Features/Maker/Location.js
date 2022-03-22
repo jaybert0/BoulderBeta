69957ddaaf65310a2afe79a47f8019635a33b36d
J Lee <jay.j.lee.1@gmail.com>
2022-03-22 13:34:21 -0400

    location form

--- client/src/Features/Maker/Location.js
+++ client/src/Features/Maker/Location.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useState, useEffect } from "react";
+import LocationForm from './LocationForm';
 
 function Location(){
 
---
+++ client/src/Features/Maker/LocationForm.js
@@ -0,0 +1,18 @@
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import * as React from "react";
+import { useState, useEffect } from "react";
+
+function LocationForm() {
+
+
+
+}
+
+export default LocationForm;
