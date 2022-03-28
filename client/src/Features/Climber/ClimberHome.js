905f4287aa9e67ce39d7c28a738d4b8aaec3e2c2
J Lee <jay.j.lee.1@gmail.com>
2022-03-28 12:56:20 -0400

    working on filters for climberhome

--- client/src/Features/Climber/ClimberHome.js
+++ client/src/Features/Climber/ClimberHome.js
@@ -1,76 +1,98 @@
-import { useState, useEffect } from 'react';
-import ProblemCard from './ProblemCard'
-import WallMap from './WallMap'
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button'
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import { render } from 'react-dom';
+import { useState, useEffect } from "react";
+import ProblemCard from "./ProblemCard";
+import WallMap from "./WallMap";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import { render } from "react-dom";
 
 // have to fetch for all problems and then filter. do not set up separate fetches on backend for difficulty. buttons filter to sort rather than fetch.
 
 function ClimberHome({
-    // setSearch, 
-    problem, getProblems, climbproblem, getClimbproblems, user
-}){
-    const [tech, setTech] = useState([]);
-    const [loc, setLoc] = useState([]);
-    useEffect(() => {
-        fetch("/teches")
-          .then((r) => r.json())
-          // .then((data) => console.log(data))
-          .then((data) => setTech(data));
-      }, []);
-    
-      useEffect(() => {
-        fetch("/locations")
-          .then((r) => r.json())
-          // .then((data) => console.log(data))
-          .then((data) => setLoc(data));
-      }, []);
+  // setSearch,
+  problem,
+  getProblems,
+  climbproblem,
+  getClimbproblems,
+  user,
+}) {
+  const [tech, setTech] = useState([]);
+  const [loc, setLoc] = useState([]);
+  useEffect(() => {
+    fetch("/teches")
+      .then((r) => r.json())
+      // .then((data) => console.log(data))
+      .then((data) => setTech(data));
+  }, []);
 
-    // const delay = ms => new Promise(res => setTimeout(res, ms))
-    // const [technique, setTechnique] = useState('');
+  useEffect(() => {
+    fetch("/locations")
+      .then((r) => r.json())
+      // .then((data) => console.log(data))
+      .then((data) => setLoc(data));
+  }, []);
+  const initholds = tech.map((prob) => prob);
+  const holds = [...new Set(initholds)];
+  const initlocs = loc.map((loc) => loc.location);
+  const locs = [...new Set(initlocs)];
+  console.log(holds);
+  console.log(locs);
 
-    // const handleChange = async (e) => {
-    //     setTechnique(e.target.value);
-    //     getProblems();
-    //     await delay(500);
-    //     setSearch(e.target.value)
-    //     console.log(e.target.value)
-    // };
-    return(
-        
-        <div>
-            {/* <WallMap id="wallmap" /> */}
-            <br></br>
-            {/* <Box sx={{ minWidth: 120 }}>
+  // const delay = ms => new Promise(res => setTimeout(res, ms))
+  // const [technique, setTechnique] = useState('');
+
+  // const handleChange = async (e) => {
+  //     setTechnique(e.target.value);
+  //     getProblems();
+  //     await delay(500);
+  //     setSearch(e.target.value)
+  //     console.log(e.target.value)
+  // };
+  return (
+    <div>
+      {/* <WallMap id="wallmap" /> */}
+      <br></br>
+      <Box sx={{ minWidth: 120 }}>
                 <FormControl fullWidth>
                     <InputLabel sx={{zIndex: -1}} id="demo-simple-select-label">Climbing Technique</InputLabel>
                     <Select
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
-                        value={technique}
+                        // value={technique}
                         label="Climbing Technique"
-                        onChange={handleChange}
+                        // onChange={handleChange}
                                     >
-                    <MenuItem value={"problem"}>All</MenuItem>
-                    <MenuItem value={"Jugs"}>Jugs</MenuItem>
-                    <MenuItem value={"Crimps"}>Crimps</MenuItem>
-                    <MenuItem value={"Pincher"}>Pincher</MenuItem>
-                    <MenuItem value={"Slopers"}>Sloper</MenuItem>
-                    <MenuItem value={"Undercling"}>Undercling</MenuItem>
-                    <MenuItem value={"Sidepull"}>Sidepull</MenuItem>
-                    <MenuItem value={"Palming"}>Palming</MenuItem>
+                        {holds.map((hold) => (
+                        <MenuItem key={hold.id} value={hold.id}>
+                        {hold.handholds}
+                    </MenuItem>
+                    ))}
                     </Select>
                 </FormControl>
-            </Box> */}
-                {/* <button onClick={() => setSearch("favorites")}>search favorite</button>
+                <FormControl fullWidth>
+                    <InputLabel sx={{zIndex: -1}} id="demo-simple-select-label">Location</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        // value={technique}
+                        label="Location"
+                        // onChange={handleChange}
+                                    >
+                      {locs.map((loc) => (
+                <MenuItem key={loc} value={loc}>
+                  {loc}
+                </MenuItem>
+              ))}
+                    </Select>
+                </FormControl>
+            </Box>
+      {/* <button onClick={() => setSearch("favorites")}>search favorite</button>
                 <button onClick={() => setSearch("in progress")}>search in progress</button>
                 <button onClick={() => setSearch("completed")}>search completed</button> */}
-                {/* <Button variant='contained' onClick={() => {
+      {/* <Button variant='contained' onClick={() => {
                     setSearch("easy");
                     setTechnique("")
                 }}>Sort Ascending Difficulty</Button>
@@ -78,13 +100,23 @@ function ClimberHome({
                     setSearch("hard");
                     setTechnique("")
                 }}>Sort Ascending Difficulty</Button> */}
-            {problem.map(problem => (
-                <ProblemCard problem={problem} user={user} alignItems="center" justifyContent="center" 
-                />
-            ))}
-        </div>
-        
-    );
+                
+      
+      
+      
+      
+      
+      
+      {problem.map((problem) => (
+        <ProblemCard
+          problem={problem}
+          user={user}
+          alignItems="center"
+          justifyContent="center"
+        />
+      ))}
+    </div>
+  );
 }
 
-export default ClimberHome
+export default ClimberHome;
