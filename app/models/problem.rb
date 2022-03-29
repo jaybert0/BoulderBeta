a4bd323d4aff6fbee02f2d6bf4c16b7088622eee
J Lee <jay.j.lee.1@gmail.com>
2022-03-29 16:48:58 -0400

    validations complete

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -3,4 +3,10 @@ class Problem < ApplicationRecord
   has_many :users, through: :climbproblems
   belongs_to :tech
   belongs_to :location
+
+  validates :difficulty, presence: true,
+  validates :grip_color, presence: true,
+  validates :end_date, presence: true,
+  validates :problem_description, length: {minimum: 10}
+
 end
